5431f8876e1fe5241eebea841d5ba729a04edec0
Mykola Horianin <anyavic2379@gmail.com>
2024-02-12 17:56:54 +0200

    first commit

---
+++ src/main/java/com/example/bookstoreweb/BookStoreWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookstoreweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookstoreweb/BookStoreWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookstoreweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
