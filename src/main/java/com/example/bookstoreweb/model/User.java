06c9aecc52a9f4413181d1c2380903339643381d
Mykola Horianin <anyavic2379@gmail.com>
2024-03-14 15:43:54 +0200

    test

--- src/main/java/com/example/bookstoreweb/model/User.java
+++ src/main/java/com/example/bookstoreweb/model/User.java
@@ -4,13 +4,9 @@ import jakarta.persistence.*;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
-
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Data
