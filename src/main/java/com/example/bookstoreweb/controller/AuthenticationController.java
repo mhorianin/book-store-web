01e590d46bca65a646100ec494c505b80f260297
Mykola Horianin <anyavic2379@gmail.com>
2024-03-18 19:07:46 +0200

    Added method login to the AuthenticationController and created AuthenticationService, JwtAuthenticationFilter, JwtUtil classes

--- src/main/java/com/example/bookstoreweb/controller/AuthenticationController.java
+++ src/main/java/com/example/bookstoreweb/controller/AuthenticationController.java
@@ -1,8 +1,11 @@
 package com.example.bookstoreweb.controller;
 
+import com.example.bookstoreweb.dto.user.UserLoginRequestDto;
+import com.example.bookstoreweb.dto.user.UserLoginResponseDto;
 import com.example.bookstoreweb.dto.user.UserRegistrationRequestDto;
 import com.example.bookstoreweb.dto.user.UserResponseDto;
 import com.example.bookstoreweb.exception.RegistrationException;
+import com.example.bookstoreweb.security.AuthenticationService;
 import com.example.bookstoreweb.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/registration")
     @Operation(summary = "Registration a new user", description = "Registration a new user")
@@ -26,5 +30,10 @@ public class AuthenticationController {
             @RequestBody @Valid UserRegistrationRequestDto registrationRequestDto
     ) throws RegistrationException {
         return userService.register(registrationRequestDto);
+    }
+
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody UserLoginRequestDto request) {
+        return authenticationService.authenticate(request);
     }
 }
---
+++ src/main/java/com/example/bookstoreweb/dto/user/UserLoginResponseDto.java
@@ -0,0 +1,4 @@
+package com.example.bookstoreweb.dto.user;
+
+public record UserLoginResponseDto(String token) {
+}
