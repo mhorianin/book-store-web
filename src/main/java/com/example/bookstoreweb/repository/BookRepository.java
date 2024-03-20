ef273df1f29ede1a93ab43f4011d865a70844768
Mykola Horianin <anyavic2379@gmail.com>
2024-03-20 13:40:12 +0200

    Added changes

--- src/main/java/com/example/bookstoreweb/repository/BookRepository.java
+++ src/main/java/com/example/bookstoreweb/repository/BookRepository.java
@@ -2,8 +2,10 @@ package com.example.bookstoreweb.repository;
 
 import com.example.bookstoreweb.model.Book;
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    @EntityGraph(attributePaths = "categories")
     List<Book> findAllByCategoriesId(Long categoryId);
 }
