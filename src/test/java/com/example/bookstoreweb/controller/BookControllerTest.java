01882de74f1852a3acd9ac0c098a1c8faa019ef7
Mykola Horianin <anyavic2379@gmail.com>
2024-04-14 17:57:51 +0300

    Added changes

--- src/test/java/com/example/bookstoreweb/controller/BookControllerTest.java
+++ src/test/java/com/example/bookstoreweb/controller/BookControllerTest.java
@@ -1,6 +1,6 @@
 package com.example.bookstoreweb.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.example.bookstoreweb.dto.book.BookDto;
@@ -27,6 +27,7 @@ import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfig
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.testcontainers.shaded.org.apache.commons.lang3.builder.EqualsBuilder;
@@ -34,8 +35,6 @@ import org.testcontainers.shaded.org.apache.commons.lang3.builder.EqualsBuilder;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BookControllerTest {
     protected static MockMvc mockMvc;
-    private static final Long ID = 1L;
-
     @Autowired
     private ObjectMapper objectMapper;
 
@@ -82,7 +81,7 @@ class BookControllerTest {
         MvcResult mvcResult = mockMvc.perform(post("/api/books")
                         .content(jsonRequest)
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
+                .andExpect(status().is2xxSuccessful())
                 .andReturn();
 
         BookDto actual = objectMapper.readValue(mvcResult.getResponse()
@@ -92,24 +91,89 @@ class BookControllerTest {
         EqualsBuilder.reflectionEquals(expected, actual, "id");
     }
 
+    @Test
+    @WithMockUser(username = "admin", authorities = {"ADMIN"})
+    @DisplayName("Return 400-th status when invalid data is entered")
+    void createBook_InvalidRequestDto_Failed() throws Exception {
+        CreateBookRequestDto requestDto = new CreateBookRequestDto();
+        requestDto.setTitle("White Fang");
+        requestDto.setIsbn("12587946831871");
+        String jsonRequest = objectMapper.writeValueAsString(requestDto);
+
+        ResultActions resultActions = mockMvc.perform(post("/api/books")
+                        .content(jsonRequest)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @WithMockUser(username = "admin", authorities = {"ADMIN"})
+    @DisplayName("Update a book by id")
+    @Sql(scripts = "classpath:database/remove-saved-book-from-db.sql",
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    void updateBook_ValidId_Success() throws Exception {
+        CreateBookRequestDto requestDto = updateBookRequestDto();
+
+        BookDto expected = new BookDto();
+        expected.setId(2L);
+        expected.setTitle(requestDto.getTitle());
+        expected.setAuthor(requestDto.getAuthor());
+        expected.setPrice(requestDto.getPrice());
+        expected.setIsbn(requestDto.getIsbn());
+        expected.setCategoryIds(requestDto.getCategories());
+
+        String jsonRequest = objectMapper.writeValueAsString(requestDto);
+
+        MvcResult mvcResult = mockMvc.perform(put("/api/books/2")
+                        .content(jsonRequest)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        BookDto actual = objectMapper.readValue(mvcResult.getResponse()
+                .getContentAsString(), BookDto.class);
+
+        Assertions.assertNotNull(actual);
+        EqualsBuilder.reflectionEquals(expected, actual, "id");
+    }
+
+    @Test
+    @WithMockUser(username = "admin", authorities = {"ADMIN"})
+    @DisplayName("Delete a book by id")
+    void deleteBook_ValidId_Success() throws Exception {
+        mockMvc.perform(delete("/api/books/1").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent())
+                .andReturn();
+    }
+
     private CreateBookRequestDto createBookRequestDto() {
         CreateBookRequestDto requestDto = new CreateBookRequestDto();
         requestDto.setTitle("White Fang");
         requestDto.setAuthor("Jack London");
         requestDto.setPrice(BigDecimal.valueOf(299));
         requestDto.setIsbn("12587946831871");
-        requestDto.setCategories(List.of(ID));
+        requestDto.setCategories(List.of(1L));
         return requestDto;
     }
 
     private BookDto createBookDto() {
         BookDto bookDto = new BookDto();
-        bookDto.setId(ID);
+        bookDto.setId(3L);
         bookDto.setTitle("White Fang");
         bookDto.setAuthor("Jack London");
         bookDto.setPrice(BigDecimal.valueOf(299));
         bookDto.setIsbn("12587946831871");
-        bookDto.setCategoryIds(List.of(ID));
+        bookDto.setCategoryIds(List.of(1L));
         return bookDto;
+    }
+
+    private CreateBookRequestDto updateBookRequestDto() {
+        CreateBookRequestDto requestDto = new CreateBookRequestDto();
+        requestDto.setTitle("Sleep");
+        requestDto.setAuthor("Taras Shevchenko");
+        requestDto.setPrice(BigDecimal.valueOf(359));
+        requestDto.setIsbn("125876332278624");
+        requestDto.setCategories(List.of(2L));
+        return requestDto;
     }
 }
